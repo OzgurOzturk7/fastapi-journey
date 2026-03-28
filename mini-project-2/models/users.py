ff8e6af58fc3dc64405c9afeff83c4c8299c6ee6
Alim Özgür Öztürk <ozgr2908@gmail.com>
2026-03-28 23:03:05 +0200

    mini-project-2: define Event and User Beanie documents

---
+++ mini-project-2/models/users.py
@@ -0,0 +1,16 @@
+from beanie import Document, Link
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from models.events import Event
+
+class User(Document):
+    email: EmailStr
+    password: str
+    events: Optional[List[Link[Event]]] = None
+
+    class Settings:
+        name = "users"
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
